af89f439d8745a0d3f90518c01fd7390944ff598
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-06-10 21:25:16 -0300

    test(Teste): teste cadastro de usuário - caso sucesso

---
+++ tests/Cadastro.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Autenticação', () => {
+
+    test('O usuário irá criar uma conta com sucesso', async ({ page }) => {
+        await page.goto('http://localhost:5173/cadastro-usuario')
+        await page.fill('input#nome', 'Usuário Teste Playwright')
+        await page.fill('input#cpf', '693.187.600-78')
+        await page.click('button[type="submit"]')
+
+        await expect(page).toHaveURL('http://localhost:5173/cadastro-usuario-auth')
+
+        await page.fill('input#email', 'teste@gmail.com')
+        await page.fill('input#senha', '412Teste#')
+        await page.fill('input#confirmaSenha', '412Teste#')
+        await page.click('button[type="submit"]')
+
+        await expect(page).toHaveURL('http://localhost:5173/login')
+        await expect(page.locator('text=Login')).toBeVisible()
+    })
+})
