b0171b5040c98c57cbfe3a4a7000b8d2c4dfb369
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-06-11 00:36:13 -0300

    test(Teste): teste de crud completo com passeios

---
+++ tests/Viagem.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Teste de Cadastro Crud Passeio', () => {
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:5173/login')
+        await page.fill('input#email', 'milena@gmail.com')
+        await page.fill('input#senha', 'TripPlanner03@')
+        await page.click('button[type="submit"]')
+        
+        await page.waitForURL('http://localhost:5173/', { timeout: 10000 })
+
+        await page.locator('.viagemCadastrada').first().click()
+    })
+
+    test('O usuário irá criar um passeio e listar com sucesso', async ({ page }) => {
+        
+        await page.goto('http://localhost:5173/info-viagem/1')
+
+        await page.getByText('Passeios').click()
+        await page.getByText('02/05').click()
+        await page.click('.adicionar')
+
+        await expect(page).toHaveURL('http://localhost:5173/cadastro-passeio/1?data=2025-05-02%2000:00:00')
+        const inputOrigem = page.locator('.input').first()
+        await inputOrigem.fill('Hotel Quirinale')
+        
+        await page.fill('input#horaInicial', '10:00')
+        await page.fill('input#horaFinal', '12:00')
+        await page.fill('input#gasto', '25')
+        await page.click('button[type="submit"]')
+
+        await expect(page.locator('text=Passeio cadastrado com sucesso!')).toBeVisible()
+        await expect(page).toHaveURL('http://localhost:5173/info-viagem/1')
+        await page.getByText('Passeios').click()
+        await page.getByText('02/05').click()
+        await expect(page).toHaveURL('http://localhost:5173/info-viagem/1')
+
+        const primeiroItem = page.locator('.listagens .icone').first()
+        await primeiroItem.click()
+        const editaPasseio = page.locator('.input').first()
+        await editaPasseio.fill('Pisa')
+        await page.click('button[type="submit"]')
+
+        await expect(page.locator('text=Passeio atualizado com sucesso!')).toBeVisible()
+        await expect(page).toHaveURL('http://localhost:5173/?data=2025-05-02%2000:00:00')
+
+        await primeiroItem.click()
+        await page.click('.excluir')
+        await page.click('.confirmar')
+        await expect(page).toHaveURL('http://localhost:5173/?data=2025-05-02%2000:00:00')
+    })
+})
