80fb87770f39138b7960c244f603b94a3c9d53f0
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-06-10 22:28:50 -0300

    test(Teste): caso falha - todos os campos são obrigatórios

--- tests/Cadastro.spec.ts
+++ tests/Cadastro.spec.ts
@@ -18,4 +18,21 @@ test.describe('Autenticação', () => {
         await expect(page).toHaveURL('http://localhost:5173/login')
         await expect(page.locator('text=Login')).toBeVisible()
     })
+
+    test('Deve mostrar todos os campos são obrigatórios', async ({ page }) => {
+        await page.goto('http://localhost:5173/cadastro-usuario')
+        await page.fill('input#nome', 'Usuário Teste Playwright')
+        await page.fill('input#cpf', '693.187.600-78')
+        await page.click('button[type="submit"]')
+
+        await expect(page).toHaveURL('http://localhost:5173/cadastro-usuario-auth')
+
+        await page.fill('input#email', '')
+        await page.fill('input#senha', '412Teste#')
+        await page.fill('input#confirmaSenha', '412Teste#')
+        await page.click('button[type="submit"]')
+
+        await expect(page).toHaveURL('http://localhost:5173/cadastro-usuario-auth')
+        await expect(page.locator('text=Todos os campos são obrigatórios')).toBeVisible()
+    })
 })
