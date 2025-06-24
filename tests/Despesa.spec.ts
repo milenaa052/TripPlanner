6a4e6dc47514603dcddacd8389106ffaf92e8ed1
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-06-24 11:56:42 -0300

    feat(Testes): Atualizando URL dos testes E2E

--- tests/Despesa.spec.ts
+++ tests/Despesa.spec.ts
@@ -3,24 +3,24 @@ import { test, expect } from '@playwright/test'
 test.describe('Teste do Crud de Despesas', () => {
 
     test.beforeEach(async ({ page }) => {
-        await page.goto('http://localhost:5173/login')
+        await page.goto('https://tripplanner.local/login')
         await page.fill('input#email', 'milena@gmail.com')
         await page.fill('input#senha', 'TripPlanner03@')
         await page.click('button[type="submit"]')
         
-        await page.waitForURL('http://localhost:5173/', { timeout: 10000 })
+        await page.waitForURL('https://tripplanner.local/', { timeout: 10000 })
 
         await page.locator('.viagemCadastrada').first().click()
     })
 
     test('O usuário irá criar, listar, atualizar e remover uma despesa com sucesso', async ({ page }) => {
         
-        await page.goto('http://localhost:5173/info-viagem/1')
+        await page.goto('https://tripplanner.local/info-viagem/1')
 
         await page.getByText('Despesas').click()
         await page.click('.adicionar')
 
-        await expect(page).toHaveURL('http://localhost:5173/cadastro-despesas/1')
+        await expect(page).toHaveURL('https://tripplanner.local/cadastro-despesas/1')
 
         await page.fill('input#tipoDespesa', 'Comida')
         await page.fill('input#gasto', '30')
@@ -28,10 +28,10 @@ test.describe('Teste do Crud de Despesas', () => {
         await page.click('button[type="submit"]')
 
         await expect(page.locator('text=Despesa cadastrada com sucesso!')).toBeVisible()
-        await expect(page).toHaveURL('http://localhost:5173/info-viagem/1')
+        await expect(page).toHaveURL('https://tripplanner.local/info-viagem/1')
         
         await page.getByText('Despesas').click()
-        await expect(page).toHaveURL('http://localhost:5173/info-viagem/1')
+        await expect(page).toHaveURL('https://tripplanner.local/info-viagem/1')
 
         const primeiroItem = page.locator('.listagens .icone').first()
         await primeiroItem.click()
@@ -42,23 +42,23 @@ test.describe('Teste do Crud de Despesas', () => {
         await page.click('button[type="submit"]')
 
         await expect(page.locator('text=Despesa atualizada com sucesso!')).toBeVisible()
-        await expect(page).toHaveURL('http://localhost:5173/info-viagem/1')
+        await expect(page).toHaveURL('https://tripplanner.local/info-viagem/1')
 
         await primeiroItem.click()
 
         await page.click('.excluir')
         await page.click('.confirmar')
         await expect(page.locator('text=Despesa excluída com sucesso!')).toBeVisible()
-        await expect(page).toHaveURL('http://localhost:5173/info-viagem/1')
+        await expect(page).toHaveURL('https://tripplanner.local/info-viagem/1')
     })
 
     test('O usuário não irá conseguir cadastrar a despesa', async ({ page }) => {
-        await page.goto('http://localhost:5173/info-viagem/1')
+        await page.goto('https://tripplanner.local/info-viagem/1')
 
         await page.getByText('Despesas').click()
         await page.click('.adicionar')
 
-        await expect(page).toHaveURL('http://localhost:5173/cadastro-despesas/1')
+        await expect(page).toHaveURL('https://tripplanner.local/cadastro-despesas/1')
 
         await page.fill('input#tipoDespesa', '')
         await page.fill('input#gasto', '30')
@@ -69,11 +69,11 @@ test.describe('Teste do Crud de Despesas', () => {
     })
 
     test('O usuário não irá conseguir atualizar a despesa', async ({ page }) => {
-        await page.goto('http://localhost:5173/info-viagem/1')
+        await page.goto('https://tripplanner.local/info-viagem/1')
 
         await page.getByText('Despesas').click()
 
-        await expect(page).toHaveURL('http://localhost:5173/info-viagem/1')
+        await expect(page).toHaveURL('https://tripplanner.local/info-viagem/1')
         const primeiroItem = page.locator('.listagens .icone').first()
         await primeiroItem.click()
         
