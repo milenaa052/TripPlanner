9e54f8aea1575b8b5a3d10b5437c07d79a2d4359
milenaa_052 <milenasantosdeoliveira40@gmail.com>
2025-06-24 22:44:47 -0300

    chore(nginx): deixando apenas o nginx exposto

--- Frontend/src/contexts/UsuarioContext.tsx
+++ Frontend/src/contexts/UsuarioContext.tsx
@@ -58,7 +58,7 @@ export const UsuarioProvider = ({ children }: { children: React.ReactNode }) => 
 
   const updateUsuario = async (updateUsuario: UpdateUsuarioPayload) => {
     try {
-      const response = await axios.put(`/api//usuario/${usuario?.idUsuario}`, updateUsuario,
+      const response = await axios.put(`/api/usuario/${usuario?.idUsuario}`, updateUsuario,
         {
           headers: {
             Authorization: `Bearer ${token}`
